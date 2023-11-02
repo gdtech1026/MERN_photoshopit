ed6664dd0ad180b471a7feac3891f9661eba04d8
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-11-02 19:44:10 -0400

    fixed auth

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,3 @@
+
+
+const { signToken, AuthenticationError } = require('../utils/auth');
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,17 +1,56 @@
-const { GraphQLError } = require('graphql');
+
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+// set token secret and expiration date
+const secret = 'mysecrettoken-noonecanknow';
 const expiration = '2h';
 
 module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
-    extensions: {
-      code: 'UNAUTHENTICATED',
-    },
-  }),
-  signToken: function ({ email, username, _id }) {
-    const payload = { email, username, _id };
+
+  authMiddleware: function (req, res, next) {
+
+    let token = req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: 'You have no token!' });
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
+    }
+
+    next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+
+// const { GraphQLError } = require('graphql');
+// const jwt = require('jsonwebtoken');
+
+// const secret = 'mysecretssshhhhhhh';
+// const expiration = '2h';
+
+// module.exports = {
+//   AuthenticationError: new GraphQLError('Could not authenticate user.', {
+//     extensions: {
+//       code: 'UNAUTHENTICATED',
+//     },
+//   }),
+//   signToken: function ({ email, username, _id }) {
+//     const payload = { email, username, _id };
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+// };
