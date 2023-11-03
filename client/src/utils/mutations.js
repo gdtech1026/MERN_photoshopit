1f899e7564208adcd9d24ad1c9e26095e147f6c1
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-11-02 21:26:42 -0400

    Mutations for front/back end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,87 @@
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+    profile {
+      _id
+      username
+      email
+      password
+      photos {
+        title
+        photoId
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+    token
+    profile {
+      _id
+      username
+      email
+      password
+      photos {
+        title
+        photoId
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+}
+`;
+
+export const SAVE_PHOTO= gql`
+
+mutation savePhoto($profileId: ID!, $photo: PhotoInput!) {
+    addPhoto(profileId: $profileId, photo: $photo) {
+      _id
+      username
+      email
+      password
+      photos {
+        title
+        photoId
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+  `;
+
+  export const REMOVE_PHOTO = gql`
+mutation RemovePhoto($photo: String!) {
+  removePhoto(photo: $photo) {
+    _id
+    username
+    email
+    password
+    photos {
+      title
+      photoId
+      authors
+      description
+      image
+      link
+    }
+  }
+}
+`;
+
+
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,45 @@
 
 
-const typeDefs = ` `
+const typeDefs = `
+
+type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    photos: [String]!
+  }
+
+  type Photo {
+    title: String!
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    adduser(name: String!, email: String!, password: String!): Auth
+
+    savePhoto(profileId: ID!, skill: String!): User
+
+    removeUserprofileId: ID!): User
+
+    removePhoto(book: String!): User
+  }
+
+
+`
 
 module.exports = typeDefs;
