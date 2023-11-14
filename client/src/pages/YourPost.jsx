4ca7c6784545b2734ed6971cb1210aefc209faea
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-11-13 19:17:49 -0500

    Undid YourPost

--- client/src/pages/YourPost.jsx
+++ client/src/pages/YourPost.jsx
@@ -1,29 +1,13 @@
-import { useParams } from 'react-router-dom';
-
 import { useQuery } from '@apollo/client';
 
 import Post from '../components/Post';
 import PostForm from '../components/PostForm';
 
-import Chat from '../components/Chat';
-import AddChat from '../components/AddChat';
-
 import { GET_PHOTO } from '../utils/queries';
 
 const YourPost = () => {
-
-    const { photoId } = useParams();
-
-    const { loading, data } = useQuery(GET_PHOTO ,{
-        variables:{
-        photoId:
-        photoId },
-    });
+    const { loading, data } = useQuery(GET_PHOTO);
     const photo = data?.photo || [];
-
-    if (loading) {
-        return <div>Loading please wait...</div>;
-      }
 
     return (
         <main>
@@ -36,30 +20,13 @@ const YourPost = () => {
                 </div>
                 <div className="col-12 col-md-8 mb-3">
                     {loading ? (
-                        <div>Loading you posts please wait...</div>
+                        <div>Loading...</div>
                     ) : (
                         <Post
                             photo={photo}
                             title="Your Posts"
                         />
                     )}
-
-                        <div>
-                            {photo.description}
-                        </div>
-
-                        <div className="p-5">
-                            <Chat comments={photo.comments} />
-                        </div>
-
-                        <p>This comment was made at {photo.date}</p>
-
-                        <p>------------------------------------------</p>
-
-                        <div>
-                            <AddChat photoId={photo._id} />
-                        </div>
-
                 </div>
             </div>
         </main>
